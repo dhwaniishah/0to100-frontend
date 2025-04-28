9029c6ef0263bd614392c82ff674717d8be6f48c
dhwaniishah <146134109+dhwaniishah@users.noreply.github.com>
2025-04-28 06:26:39 +0000

    Modified cite config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages')
+    }
+  }
 })
